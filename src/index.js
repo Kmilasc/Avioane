5946c19d55ac05fb1dd4696b70787b3098d6f531
Kmilasc <kamillaandrade77@gmail.com>
2025-06-02 12:36:31 -0300

    Refatoracao

--- src/index.js
+++ src/index.js
@@ -52,6 +52,10 @@ function resetGame() {
 	document.getElementById("setupTable").style.display = "";
 	document.getElementById("playerZone").innerHTML = "";
 	document.getElementById("computerZone").innerHTML = "";
+	playerLives = 5;
+	computerLives = 5;
+	document.getElementById("playerLives").innerHTML = playerLives;
+	document.getElementById("computerLives").innerHTML = computerLives;
 	preview();
 }
 
@@ -77,78 +81,99 @@ function startGame() {
 	displayGrid(false);
 }
 
+function showMessage(msg) {
+    let box = document.getElementById("messageBox");
+    if (!box) {
+        box = document.createElement("div");
+        box.id = "messageBox";
+        box.style.display = "none";
+        box.style.position = "fixed";
+        box.style.top = "10px";
+        box.style.left = "50%";
+        box.style.transform = "translateX(-50%)";
+        box.style.background = "#222";
+        box.style.color = "#fff";
+        box.style.padding = "10px 20px";
+        box.style.borderRadius = "5px";
+        box.style.zIndex = "1000";
+        document.body.appendChild(box);
+    }
+    box.innerText = msg;
+    box.style.display = "block";
+    setTimeout(() => { box.style.display = "none"; }, 4000);
+}
 
 function initializePlayer(preview) {
-	console.log("initializePlayer/0")
-	let x, y, j, p, varfx, varfy, nrAvion = 0, orientare;
+    console.log("initializePlayer/0")
+    let x, y, j, p, varfx, varfy, nrAvion = 0, orientare;
 
-	let grid = [];
-	for (y = 0; y < gridX; ++y) {
-		grid[y] = [];
-		for (x = 0; x < gridX; ++x)
-			grid[y][x] = [imagesIds.Unknown, -1, 0];
-	}
+    let grid = [];
+    for (y = 0; y < gridX; ++y) {
+        grid[y] = [];
+        for (x = 0; x < gridX; ++x)
+            grid[y][x] = [imagesIds.Unknown, -1, 0];
+    }
 
-	playerLives = 0;
+    playerLives = 0;
 
-	for (j = 1; j < 6; j++) {
-		varfx = document.getElementById("pozx" + j).value;
-		varfy = document.getElementById("pozy" + j).value;
+    for (j = 1; j < 6; j++) {
+        varfx = document.getElementById("pozx" + j).value;
+        varfy = document.getElementById("pozy" + j).value;
 
-		if (varfx == "" || varfy == "") {
-			if (!preview) {
-				alert("Fill in the board with 5 planes!!!");
-				return false;
-			}
-			else
-				continue;
-		}
+        if (varfx == "" || varfy == "") {
+            if (!preview) {
+                showMessage("Fill in the board with 5 planes!!!");
+                return false;
+            }
+            else
+                continue;
+        }
 
-		varfx = parseInt(varfx) - 1;
-		varfy = parseInt(varfy) - 1;
+        varfx = parseInt(varfx) - 1;
+        varfy = parseInt(varfy) - 1;
 
-		orientare = parseInt(document.getElementById("dir" + j).value);
+        orientare = parseInt(document.getElementById("dir" + j).value);
 
-		// checking the position of the airplane
+        // checking the position of the airplane
 
-		if (varfx < 0 || varfx >= gridX || varfy < 0 || varfy >= gridY || grid[varfy][varfx][0] == "a") {
-			alert("Plane " + j + " was not set right!!!");
-			document.getElementById("pozx" + j).focus();
-			return false;
-		}
+        if (varfx < 0 || varfx >= gridX || varfy < 0 || varfy >= gridY || grid[varfy][varfx][0] == "a") {
+            showMessage("Plane " + j + " was not set right!!!");
+            document.getElementById("pozx" + j).focus();
+            return false;
+        }
 
-		for (p = 0; p < 9; p++) {
-			let pozx = positions[orientare][p][0] + varfx;
-			let pozy = positions[orientare][p][1] + varfy;
+        for (p = 0; p < 9; p++) {
+            let pozx = positions[orientare][p][0] + varfx;
+            let pozy = positions[orientare][p][1] + varfy;
 
-			if (pozx >= gridX || pozx < 0 || pozy >= gridY || pozy < 0) {
-				alert("Plane " + j + " was not set right!!!");
-				document.getElementById("pozx" + j).focus();
-				return false;
-			}
+            if (pozx >= gridX || pozx < 0 || pozy >= gridY || pozy < 0) {
+                showMessage("Plane " + j + " was not set right!!!");
+                document.getElementById("pozx" + j).focus();
+                return false;
+            }
 
-			if (grid[pozy][pozx][0] == "a") {
-				alert("Plane " + j + " overlaps another plane!!!");
-				document.getElementById("pozx" + j).focus();
-				return false;
-			}
-		}
+            if (grid[pozy][pozx][0] == "a") {
+                showMessage("Plane " + j + " overlaps another plane!!!");
+                document.getElementById("pozx" + j).focus();
+                return false;
+            }
+        }
 
-		for (p = 0; p < 9; p++) {
-			let pozx = positions[orientare][p][0] + varfx;
-			let pozy = positions[orientare][p][1] + varfy;
-			grid[pozy][pozx][0] = "a";
-			grid[pozy][pozx][1] = nrAvion;
-		}
-		grid[varfy][varfx][0] = "a";
-		grid[varfy][varfx][1] = nrAvion;
-		grid[varfy][varfx][2] = "cap";
+        for (p = 0; p < 9; p++) {
+            let pozx = positions[orientare][p][0] + varfx;
+            let pozy = positions[orientare][p][1] + varfy;
+            grid[pozy][pozx][0] = "a";
+            grid[pozy][pozx][1] = nrAvion;
+        }
+        grid[varfy][varfx][0] = "a";
+        grid[varfy][varfx][1] = nrAvion;
+        grid[varfy][varfx][2] = "cap";
 
-		playerAirplanes[nrAvion] = 10;
-		playerLives++;
-		nrAvion++;
-	}
-	return grid;
+        playerAirplanes[nrAvion] = 10;
+        playerLives++;
+        nrAvion++;
+    }
+    return grid;
 }
 
 function initializeComputer() {
@@ -231,19 +256,19 @@ function displayGrid(isComputer) {
 	s += '</div>';
 	if (isComputer) {
 		for (y = 0; y < gridY; y++) {
-      s += '<div class="row">'
+			s += '<div class="row">'
 			for (x = 0; x < gridX; x++) {
 				s += '<button onclick="javascript:gridClick(' + y + ',' + x + ');"><img class="img-grid" name="pc' + y + '_' + x + '" src="src/assets/ave.svg" width=16 height=16><div class="overlay"></div></button>';
 			}
-      s += '<div class="number-y">' + (y + 1) + '</div>';
+			s += '<div class="number-y">' + (y + 1) + '</div>';
 			s += '</div>';
 		}
 	} else {
 		for (y = 0; y < gridY; y++) {
-      s += '<div class="row">'
+			s += '<div class="row">'
 			s += '<div class="number-y">' + (y + 1) + '</div>';
 			for (x = 0; x < gridX; x++) {
-				s += '<button onclick="javascript:playerGridClick(' + y + ',' + x + ');"><img class="img-grid" name="ply' + y + '_' + x + '" src="src/assets/av' + player[y][x][0] + '.svg" width=16 height=16><div class="overlay"></div></button>';
+				s += '<button onmouseover="javascript:previewHover(' + y + ',' + x + ');" onmouseout="javascript:clearPreview();" onclick="javascript:playerGridClick(' + y + ',' + x + ');"><img class="img-grid" name="ply' + y + '_' + x + '" src="src/assets/av' + player[y][x][0] + '.svg" width=16 height=16><div class="overlay"></div></button>';
 			}
 			s += '</div>';
 		}
@@ -265,111 +290,111 @@ function playerGridClick(y, x) {
 }
 
 function gridClick(y, x) {
-	console.log("gridClick/2")
-	if (playerTurn) {
-		if ((computer[y][x][2] == "cap") && (computer[y][x][0] == "a")) {
-			let nrAvion = computer[y][x][1];
-			computerAirplanes[nrAvion] = 0;
-			shootDownAirplane(computer, nrAvion, true);
-			alert("You struck down one of the opponent's planes!!!");
-			if (--computerLives == 0) {
-				alert("YOU WON!!! CONGRATS :)");
-				playerScore++;
-				document.getElementById("playerScore").innerHTML = playerScore;
-				document.getElementById("replayButton").style.display = "";
-				playerTurn = false;
-			}
-			document.getElementById("computerLives").innerHTML = computerLives;
-		} else
-			if (computer[y][x][0] == "a") {
-				setImage(y, x, imagesIds.Taken, true);
-				let nrAvion = computer[y][x][1];
-				if (--computerAirplanes[nrAvion] == 0) {
-					shootDownAirplane(computer, nrAvion, true);
-					alert("YOUR AIRPLANE WAS SHOT DOWN!!!");
-					if (--computerLives == 0) {
-						alert("YOU LOST!!! :(");
-						playerTurn = false;
-						playerScore++;
-						document.getElementById("playerScore").innerHTML = playerScore;
-						document.getElementById("replayButton").style.display = "";
-					}
-					document.getElementById("computerLives").innerHTML = computerLives;
-				}
+    console.log("gridClick/2")
+    if (playerTurn) {
+        if ((computer[y][x][2] == "cap") && (computer[y][x][0] == "a")) {
+            let nrAvion = computer[y][x][1];
+            computerAirplanes[nrAvion] = 0;
+            shootDownAirplane(computer, nrAvion, true);
+            showMessage("You struck down one of the opponent's planes!!!");
+            if (--computerLives == 0) {
+                showMessage("YOU WON!!! CONGRATS :)");
+                playerScore++;
+                document.getElementById("playerScore").innerHTML = playerScore;
+                document.getElementById("replayButton").style.display = "";
+                playerTurn = false;
+            }
+            document.getElementById("computerLives").innerHTML = computerLives;
+        } else
+            if (computer[y][x][0] == "a") {
+                setImage(y, x, imagesIds.Taken, true);
+                let nrAvion = computer[y][x][1];
+                if (--computerAirplanes[nrAvion] == 0) {
+                    shootDownAirplane(computer, nrAvion, true);
+                    showMessage("YOUR AIRPLANE WAS SHOT DOWN!!!");
+                    if (--computerLives == 0) {
+                        showMessage("YOU LOST!!! :(");
+                        playerTurn = false;
+                        playerScore++;
+                        document.getElementById("playerScore").innerHTML = playerScore;
+                        document.getElementById("replayButton").style.display = "";
+                    }
+                    document.getElementById("computerLives").innerHTML = computerLives;
+                }
 
 
-				if (playerTurn) computerGridClick();
-			}
-			else if (computer[y][x][0] == "e") {
-				setImage(y, x, imagesIds.Marked, true);
-				computerGridClick();
-			}
-	}
+                if (playerTurn) computerGridClick();
+            }
+            else if (computer[y][x][0] == "e") {
+                setImage(y, x, imagesIds.Marked, true);
+                computerGridClick();
+            }
+    }
 }
 
 function computerGridClick() {
-	console.log("computerGridClick/0")
-	let x, y, selectedX, selectedY;
-	let selected = false;
+    console.log("computerGridClick/0")
+    let x, y, selectedX, selectedY;
+    let selected = false;
 
-	for (y = 0; y < gridY && !selected; ++y) {
-		for (x = 0; x < gridX && !selected; ++x) {
-			if (player[y][x][0] == "t") {
-				selectedX = x; selectedY = y;
+    for (y = 0; y < gridY && !selected; ++y) {
+        for (x = 0; x < gridX && !selected; ++x) {
+            if (player[y][x][0] == "t") {
+                selectedX = x; selectedY = y;
 
-				if ((x - 1 >= 0) && ((player[y][x - 1][0] == "a") || (player[y][x - 1][0] == "e"))) {
-					selectedX = x - 1;
-					selected = true;
-				} else if ((x + 1 < gridX) && ((player[y][x + 1][0] == "a") || (player[y][x + 1][0] == "e"))) {
-					selectedX = x + 1;
-					selected = true;
-				} else if ((y - 1 >= 0) && ((player[y - 1][x][0] == "a") || (player[y - 1][x][0] == "e"))) {
-					selectedY = y - 1;
-					selected = true;
-				} else if ((y + 1 < gridY) && ((player[y + 1][x][0] == "a") || (player[y + 1][x][0] == "e"))) {
-					selectedY = y + 1;
-					selected = true;
-				}
-			}
-		}
-	}
+                if ((x - 1 >= 0) && ((player[y][x - 1][0] == "a") || (player[y][x - 1][0] == "e"))) {
+                    selectedX = x - 1;
+                    selected = true;
+                } else if ((x + 1 < gridX) && ((player[y][x + 1][0] == "a") || (player[y][x + 1][0] == "e"))) {
+                    selectedX = x + 1;
+                    selected = true;
+                } else if ((y - 1 >= 0) && ((player[y - 1][x][0] == "a") || (player[y - 1][x][0] == "e"))) {
+                    selectedY = y - 1;
+                    selected = true;
+                } else if ((y + 1 < gridY) && ((player[y + 1][x][0] == "a") || (player[y + 1][x][0] == "e"))) {
+                    selectedY = y + 1;
+                    selected = true;
+                }
+            }
+        }
+    }
 
-	if (!selected) {
-		do {
-			selectedY = Math.floor(Math.random() * (gridY - 1));
-			selectedX = Math.floor(Math.random() * (gridX - 1));
-		} while ((player[selectedY][selectedX][0] == "t") || (player[selectedY][selectedX][0] == "d") || (player[selectedY][selectedX][0] == "m"));
-	}
+    if (!selected) {
+        do {
+            selectedY = Math.floor(Math.random() * (gridY - 1));
+            selectedX = Math.floor(Math.random() * (gridX - 1));
+        } while ((player[selectedY][selectedX][0] == "t") || (player[selectedY][selectedX][0] == "d") || (player[selectedY][selectedX][0] == "m"));
+    }
 
-	if ((player[selectedY][selectedX][2] == "cap") || (player[selectedY][selectedX][0] == "a")) {
-		setImage(selectedY, selectedX, imagesIds.Taken, false);
-		let nrAvion = player[selectedY][selectedX][1];
-		if (player[selectedY][selectedX][2] == "cap") {
-			playerAirplanes[nrAvion] = 1;
-		}
-		if (--playerAirplanes[nrAvion] == 0) {
-			shootDownAirplane(player, nrAvion, false);
-			alert("One of your planes was struck down!!!");
-			playerLostLive()
-		}
-	} else if (player[selectedY][selectedX][0] == "e") {
-		setImage(selectedY, selectedX, imagesIds.Marked, false);
-	}
+    if ((player[selectedY][selectedX][2] == "cap") || (player[selectedY][selectedX][0] == "a")) {
+        setImage(selectedY, selectedX, imagesIds.Taken, false);
+        let nrAvion = player[selectedY][selectedX][1];
+        if (player[selectedY][selectedX][2] == "cap") {
+            playerAirplanes[nrAvion] = 1;
+        }
+        if (--playerAirplanes[nrAvion] == 0) {
+            shootDownAirplane(player, nrAvion, false);
+            showMessage("One of your planes was struck down!!!");
+            playerLostLive()
+        }
+    } else if (player[selectedY][selectedX][0] == "e") {
+        setImage(selectedY, selectedX, imagesIds.Marked, false);
+    }
 
-	document.getElementById("playerLives").innerHTML = playerLives;
+    document.getElementById("playerLives").innerHTML = playerLives;
 }
 
 function playerLostLive() {
-	console.log("playerLostLive/0")
-	if (--playerLives == 0) {
-		displayEnemyAirplanes();
-		alert("YOU LOST :(\n");
-		playerTurn = false;
+    console.log("playerLostLive/0")
+    if (--playerLives == 0) {
+        displayEnemyAirplanes();
+        showMessage("YOU LOST :(\n");
+        playerTurn = false;
 
-		computerScore++;
-		document.getElementById("computerScore").innerHTML = computerScore;
-		document.getElementById("replayButton").style.display = "";
-	}
+        computerScore++;
+        document.getElementById("computerScore").innerHTML = computerScore;
+        document.getElementById("replayButton").style.display = "";
+    }
 }
 
 function shootDownAirplane(grid, nrAvion, isComputer) {
@@ -397,4 +422,54 @@ function displayEnemyAirplanes() {
 				setImage(y, x, imagesIds.Airplane, true);
 		}
 	}
+}
+
+function validateCoordinateInput(e) {
+	const input = e;
+	const value = parseInt(input.value);
+	const max = parseInt(input.getAttribute("max"));
+	const min = parseInt(input.getAttribute("min"));
+
+    if (isNaN(value) || value < min || value > max) {
+		input.value = "";
+		showMessage(`Please enter a number between ${min} and ${max}.`);
+		input.focus();
+	} else {
+		preview();
+	}
+
+}
+
+function previewHover(y, x) {
+    if (!canEditGrid || !selectedAirplane) return;
+    
+    let orientare = parseInt(document.getElementById("dir" + selectedAirplane).value);
+    let previewCells = [];
+    
+    // Add the head position
+    previewCells.push([y, x]);
+    
+    // Add the airplane body positions
+    for (let p = 0; p < 9; p++) {
+        let pozx = positions[orientare][p][0] + x;
+        let pozy = positions[orientare][p][1] + y;
+        
+        if (pozx >= 0 && pozx < gridX && pozy >= 0 && pozy < gridY) {
+            previewCells.push([pozy, pozx]);
+        }
+    }
+    
+    // Apply preview style to all cells
+    previewCells.forEach(([py, px]) => {
+        let button = document.querySelector(`button[onclick*="playerGridClick(${py},${px})"]`);
+        if (button) {
+            button.classList.add('preview-hover');
+        }
+    });
+}
+
+function clearPreview() {
+    document.querySelectorAll('.preview-hover').forEach(element => {
+        element.classList.remove('preview-hover');
+    });
 }
